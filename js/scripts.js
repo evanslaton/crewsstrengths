81fa6d8136888ee80d3aa4a20af3e86cee816a65
Evan Slaton <evanslaton@gmail.com>
2018-04-23 11:39:08 -0400

    Added blog index page

--- js/scripts.js
+++ js/scripts.js
@@ -6,42 +6,49 @@ $(document).ready(function() {
   var davidR = {
     name: 'David R. ',
     src: "img/testimonials/david-r.png",
-    testimonial: " After working with Donald, I recognize behaviors I used to identify as character faults, as expressions of the gifts with which I am created and wired. This has proved extremely valuable because I have leveraged those gifts to create real results.  I've experienced a change in my marriage, my relationship with my son, and especially things around building my business and working with clients."
+    src2: "../img/testimonials/david-r.png",
+    testimonial: " After working with Donald, I recognize behaviors I used to identify as character faults, as expressions of the gifts with which I am created and wired. This has proved extremely valuable because I have leveraged those gifts to create real results.  I\'ve experienced a change in my marriage, my relationship with my son, and especially things around building my business and working with clients."
   };
 
   var jimA = {
     name: 'Jim A. ',
     src: "img/testimonials/jim-a.png",
-    testimonial: " I strongly recommend Donald's coaching. Donald helped me identify the working dynamics of my StrengthFinder 2.0 assessment results. In only a few weeks, I had several job interviews and at least one offer. I credit Donald's great work for this turnaround and would recommend his coaching to any individual or organization seeking to get maximum yield from the Strengths assessment."
+    src2: "../img/testimonials/jim-a.png",
+    testimonial: " I strongly recommend Donald\'s coaching. Donald helped me identify the working dynamics of my StrengthFinder 2.0 assessment results. In only a few weeks, I had several job interviews and at least one offer. I credit Donald\'s great work for this turnaround and would recommend his coaching to any individual or organization seeking to get maximum yield from the Strengths assessment."
   };
 
   var justinR = {
     name: 'Justin R. ',
     src: "img/testimonials/justin-r.png",
-    testimonial: " It wasn’t until meeting with Donald for 1-on-1 coaching that I began to internalize what my StrengthsFinder assessment results really meant.  Becoming more self-aware has been truly powerful. If you’re looking to tap into your strengths, I’d highly recommend working with Donald I. Crews."
+    src2: "../img/testimonials/justin-r.png",
+    testimonial: " It wasn\'t until meeting with Donald for 1\-on\-1 coaching that I began to internalize what my StrengthsFinder assessment results really meant.  Becoming more self\-aware has been truly powerful. If you\'re looking to tap into your strengths, I\'d highly recommend working with Donald I. Crews."
   };
 
   var deniseT = {
   name: 'Denise T. ',
   src: "img/testimonials/denise-t.png",
-  testimonial: " In working with Donald, I’ve learned (and continue to learn) how to 'name, claim, and aim' my strengths.  If you’ve been spinning your wheels trying to figure-out what you want to be when you grow up, you’ll want to reach-out to Donald ASAP!"
+  src2: "../img/testimonials/denise-t.png",
+  testimonial: " In working with Donald, I\'ve learned \(and continue to learn\) how to \'name, claim, and aim\' my strengths.  If you\'ve been spinning your wheels trying to figure\-out what you want to be when you grow up, you\'ll want to reach\-out to Donald ASAP\!"
   };
 
   var cynthiaD = {
   name: 'Cynthia D. ',
   src: "img/testimonials/cynthia-d.png",
-  testimonial: " I think we should bottle Donald's enthusiasm and sip on it throughout our days."
+  src2: "../img/testimonials/cynthia-d.png",
+  testimonial: " I think we should bottle Donald\'s enthusiasm and sip on it throughout our days."
   };
 
   var dedraP = {
   name: 'Dedra P. ',
   src: "img/testimonials/dedra-p.png",
-  testimonial: " Donald is not only an expert in his field, but a 'top-notch' presenter. I've had the opportunity to attend several of Donald's presentations and continue to be overwhelmed by his level of knowledge as well as engagement. He is a true resource that provides expertise and dedication to his craft."
+  src2: "../img/testimonials/dedra-p.png",
+  testimonial: " Donald is not only an expert in his field, but a \'top\-notch\' presenter. I\'ve had the opportunity to attend several of Donald\'s presentations and continue to be overwhelmed by his level of knowledge as well as engagement. He is a true resource that provides expertise and dedication to his craft."
   };
 
   var annR = {
   name: 'Ann R. ',
   src: "img/testimonials/ann-r.png",
+  src2: "../img/testimonials/ann-r.png",
   testimonial: " Donald is a wonderful presenter. He brings his subject matter to life by sharing real world examples audiences can relate to and understand. I have heard numerous positive comments after his presentations. Participants appreciate his depth of knowledge and personable delivery."
   };
 
@@ -72,6 +79,17 @@ $(document).ready(function() {
   $('#testimonial2').text(testimonials[num2].testimonial)
   $('#testimonial2').prepend(`<span class="name">${testimonials[num2].name} -</span>`);
 
+  //Testimonial 3
+  $('#testimonial-img3').attr('src', testimonials[num1].src2);
+  $('#testimonial1').text(testimonials[num1].testimonial)
+  $('#testimonial1').prepend(`<span class="name">${testimonials[num1].name} -</span>`);
+
+
+  //Testimonial 4
+  $('#testimonial-img4').attr('src', testimonials[num2].src2);
+  $('#testimonial2').text(testimonials[num2].testimonial)
+  $('#testimonial2').prepend(`<span class="name">${testimonials[num2].name} -</span>`);
+
   let i = -1;
 
   var showText = () => {
@@ -82,8 +100,7 @@ $(document).ready(function() {
       i = 0;
     }
 
-      console.log(i);
-    $(evilForce[i]).fadeIn(2000).delay(2000).fadeOut(2000, showText);
+    $(evilForce[i]).fadeIn(1300).delay(800).fadeOut(1300, showText);
   }
 
   showText();
